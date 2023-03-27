5d304f2fe00ecd602e04f383c957bdf1ea1d3921
PauloAbrahao <ab.cod@outlook.com>
2023-03-27 19:43:47 -0300

    feat: added some responsiveness rules, created an ImageDescription key in the Post object in data.ts

--- dist/data.js
+++ dist/data.js
@@ -5,6 +5,7 @@ const posts = [
         id: 1,
         title: "Post 1",
         imageUrl: "https://cdn.pixabay.com/photo/2023/03/21/12/19/mountains-7867137_960_720.jpg",
+        imageDescription: "Some mountains and hills, a beautiful landscape",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae.",
         read_more: "Read More",
         link_to: "#"
@@ -13,6 +14,7 @@ const posts = [
         id: 2,
         title: "Post 2",
         imageUrl: "https://cdn.pixabay.com/photo/2023/03/14/12/41/wheat-7852286_960_720.jpg",
+        imageDescription: "A wheat plantation",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae.",
         read_more: "Read More",
         link_to: "#"
@@ -21,6 +23,7 @@ const posts = [
         id: 3,
         title: "Post 3",
         imageUrl: "https://cdn.pixabay.com/photo/2023/03/02/22/11/belgium-7826588_960_720.jpg",
+        imageDescription: "Some buildings in a metropolis",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt.",
         read_more: "Read More",
         link_to: "#"
@@ -29,6 +32,7 @@ const posts = [
         id: 4,
         title: "Post 4",
         imageUrl: "https://cdn.pixabay.com/photo/2022/12/18/20/09/christmas-market-7664171_960_720.jpg",
+        imageDescription: "A beautiful city at night, with an amusement park",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt.",
         read_more: "Read More",
         link_to: "#"
@@ -39,10 +43,10 @@ function setDataOfPosts() {
     posts.map((post) => {
         postComponent += `
     <div class="post" id="post">
-      <img src=${post.imageUrl} alt="">
+      <img src="${post.imageUrl}" alt="${post.imageDescription}">
       <h3 class="post-title" id="post_title">${post.title}</h3>
       <p class="post-content" id="post-content">${post.body}</p>
-      <a href=${post.link_to} class="post-link">${post.read_more}</a>
+      <a href="${post.link_to}" class="post-link">${post.read_more}</a>
     </div>`;
     });
     blogList.innerHTML = postComponent;
--- src/data.ts
+++ src/data.ts
@@ -5,6 +5,7 @@ interface PostsData {
   id: number;
   title: string;
   imageUrl: string;
+  imageDescription: string;
   body: string;
   read_more: string;
   link_to: string;
@@ -15,6 +16,7 @@ const posts: PostsData[] = [
     id: 1,
     title: "Post 1",
     imageUrl: "https://cdn.pixabay.com/photo/2023/03/21/12/19/mountains-7867137_960_720.jpg",
+    imageDescription: "Some mountains and hills, a beautiful landscape",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae.",
     read_more: "Read More",
     link_to: "#"
@@ -23,6 +25,7 @@ const posts: PostsData[] = [
     id: 2,
     title: "Post 2",
     imageUrl: "https://cdn.pixabay.com/photo/2023/03/14/12/41/wheat-7852286_960_720.jpg",
+    imageDescription: "A wheat plantation",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae.",
     read_more: "Read More",
     link_to: "#"
@@ -31,6 +34,7 @@ const posts: PostsData[] = [
     id: 3,
     title: "Post 3",
     imageUrl: "https://cdn.pixabay.com/photo/2023/03/02/22/11/belgium-7826588_960_720.jpg",
+    imageDescription: "Some buildings in a metropolis",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt.",
     read_more: "Read More",
     link_to: "#"
@@ -39,6 +43,7 @@ const posts: PostsData[] = [
     id: 4,
     title: "Post 4",
     imageUrl: "https://cdn.pixabay.com/photo/2022/12/18/20/09/christmas-market-7664171_960_720.jpg",
+    imageDescription: "A beautiful city at night, with an amusement park",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt. Quisquam, quae. Quisquam voluptas, quod, quia, quae voluptates quibusdam voluptatibus voluptatum quidem quos quas nesciunt.",
     read_more: "Read More",
     link_to: "#"
@@ -46,16 +51,17 @@ const posts: PostsData[] = [
   
 ];
 
+// Function to set the data of the posts array to the DOM
 function setDataOfPosts() {
   let postComponent = "";
 
   posts.map((post) => {
     postComponent += `
     <div class="post" id="post">
-      <img src=${post.imageUrl} alt="">
+      <img src="${post.imageUrl}" alt="${post.imageDescription}">
       <h3 class="post-title" id="post_title">${post.title}</h3>
       <p class="post-content" id="post-content">${post.body}</p>
-      <a href=${post.link_to} class="post-link">${post.read_more}</a>
+      <a href="${post.link_to}" class="post-link">${post.read_more}</a>
     </div>`;
   });
 
